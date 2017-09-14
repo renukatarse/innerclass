6f640526964a9a6b5eac44eb4d8bc81ac500ff1a
renuka <renutarse1@gmail.com>
2017-09-14 16:12:57 +0530

    inner commit

---
+++ inner class/src/My_class.java
@@ -0,0 +1,16 @@
+
+public class My_class {
+	
+
+		   public static void main(String args[]) {
+		      // Instantiating the outer class 
+		      Outer_Demo outer = new Outer_Demo();
+		      
+		      // Accessing the display_Inner() method.
+		      outer.display_Inner();
+		   }
+
+	}
+
+
+
